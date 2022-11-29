6fa624b3c6d7b78cbb2f1f9821751b43b7319ea1
Tabish Raza <108555503+webstwk@users.noreply.github.com>
2022-11-30 01:36:05 +0530

    Add files via upload

---
+++ java_method.java
@@ -0,0 +1,28 @@
+public class java_method {
+    static int logic (int x,int y)
+    {
+        int z;
+        if(x>y)
+        {
+            z=x+y;
+        }
+        else
+        {
+            z=(x+y)*5;
+        }
+        return z;
+    }
+
+    public static void main(String[] args) {
+        int a=5;
+        int b=8;
+        int c;
+        c= logic(a,b);
+        int a1=6;
+        int b1=2;
+        int c1;
+        c1=logic(a1,b1);
+        System.out.println(c);
+        System.out.println(c1);
+    }
+}
